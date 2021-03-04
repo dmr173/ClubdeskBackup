867fc0b9b387cd466dd3b0c6c07e5cb1024cceac
dmr173 <dm.rossel@gmail.com>
2021-03-04 17:17:09 +0100

    Working Version Docker Image

--- clubdesk.py
+++ clubdesk.py
@@ -135,7 +135,7 @@ prep_folder()
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--log-level=1")  # 3=fatal only
-chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("window-size=1920x1080")
 chrome_options.add_experimental_option("prefs", {
